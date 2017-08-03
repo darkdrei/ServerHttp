5d8ca1887f9fecdaa103e623ac8bea4581fb1212
darkdrei <darkdrei88@gmail.com>
2017-08-02 19:28:07 -0500

    Handler de peticiones

---
+++ src/serverexile/ServerExile.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serverexile;
+
+import logica.impresion.Imprimir;
+
+/**
+ *
+ * @author dark
+ */
+public class ServerExile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Imprimir i = new Imprimir();
+        i.main();
+    }
+    
+}
